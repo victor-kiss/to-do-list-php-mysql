ad88b14291cce425a5ffe29f1ab8495635113542
victor-kiss <victorkiss10@gmail.com>
2023-07-14 11:56:35 -0300

    archive for database connection

---
+++ db-connect.php
@@ -0,0 +1,9 @@
+<?php
+// values for db connection (local)
+$host = "localhost";
+$port = "3306";
+$user = "your-username";
+$password = "your-password";
+$dbname = "database-name";
+$dsn = "mysql:host=$host;port=$port;dbname=$dbname";
+?>
